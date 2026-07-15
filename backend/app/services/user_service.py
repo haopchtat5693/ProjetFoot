7f44215e8dd7d5ff1187d32d7e02cf462cf2fe78
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-14 23:03:00 -0400

    feat: implement create_new_user function in user_service.py

---
+++ backend/app/services/user_service.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from app import schemas, crud
+from app.core.security import get_password_hash 
+
+def create_new_user(db: Session, user_in: schemas.UserCreate):
+
+    user_data = user_in.model_dump() 
+    
+    password = user_data.pop("password")
+    user_data["hashed_password"] = get_password_hash(password)
+    
+    return crud.user.create_user(db, user_data)
