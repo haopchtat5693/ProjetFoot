c175283ee014375806ab843c2733481972013148
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-16 18:53:17 -0400

    feat: implement access token creation with expiration handling

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,12 @@
+from datetime import datetime, timedelta, timezone
+import os
+from jose import jwt
 from passlib.context import CryptContext
+from dotenv import load_dotenv
+
+load_dotenv()  
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +15,9 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
