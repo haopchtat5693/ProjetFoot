935599bf515dd1d5e6a67023e28f93f530822a78
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-17 17:59:18 -0400

    refactor: update user dependency management to use core dependencies

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from app import schemas, crud
 from app.services import user_service
 from app.database import get_db
-from app.services.auth_service import get_current_user
+from app.core.deps import get_current_user, get_current_admin, get_owner_or_admin
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -23,7 +23,10 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
     return user
 
 @router.get("/" , response_model=list[schemas.User])
-def get_users(db: Session = Depends(get_db)):
+def get_users(
+    db: Session = Depends(get_db),
+    current_admin: schemas.User = Depends(get_current_admin),
+):
     return user_service.get_users(db)
 
 @router.put("/{user_id}", response_model=schemas.User)
@@ -31,10 +34,8 @@ def update_user(
     user_id: int, 
     user_in: schemas.UserUpdate, 
     db: Session = Depends(get_db),
-    current_user: schemas.User = Depends(get_current_user) 
+    _: schemas.User = Depends(get_owner_or_admin)
 ):
-    if current_user.id != user_id:
-        raise HTTPException(status_code=403, detail="Vous n'avez pas le droit de modifier ce compte")
     
     user = crud.user.get_user(db, user_id)
     if not user:
@@ -45,11 +46,9 @@ def update_user(
 def delete_user(
     user_id: int, 
     db: Session = Depends(get_db),
-    current_user: schemas.User = Depends(get_current_user) 
+    _: schemas.User = Depends(get_owner_or_admin)
 ):
-    if current_user.id != user_id:
-        raise HTTPException(status_code=403, detail="Vous n'avez pas le droit de supprimer ce compte")
-        
+
     user = crud.user.get_user(db, user_id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
