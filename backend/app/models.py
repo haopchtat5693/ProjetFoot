2856014a6e1bd61b7963e1c679b6d458dfe77915
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-17 17:58:45 -0400

    feat: change user role field from is_admin to role with default value "viewer"

--- backend/app/models.py
+++ backend/app/models.py
@@ -107,7 +107,7 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    is_admin = Column(Boolean, default=False)
+    role = Column(String, default="viewer")
 
     tokens = relationship("Token", back_populates="user")
 
