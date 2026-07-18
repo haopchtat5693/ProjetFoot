86449f0e80405d016faf371c25e4fcb750b26702
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-18 19:53:14 -0400

    feat: rename player stats functions and update CRUDBase to use PlayerMatchStats

--- backend/app/crud/player_stats_crud.py
+++ backend/app/crud/player_stats_crud.py
@@ -5,28 +5,37 @@ from app import models, schemas
 from .base import CRUDBase
 
 
-player_stats_crud = CRUDBase(models.PlayerStats)
+player_stats_crud = CRUDBase(models.PlayerMatchStats)
 
 
-def getPlayerStats(db: Session, player_stats_id: int):
+def getPlayerMatchStats(db: Session, player_stats_id: int):
     return player_stats_crud.get(db, player_stats_id)
 
 
-def getPlayerStatsList(db: Session, skip: int = 0, limit: int = 100):
+def getPlayerMatchStatsList(db: Session, skip: int = 0, limit: int = 100):
     return player_stats_crud.get_multi(db, skip=skip, limit=limit)
 
+def get_stats_by_player_and_season(db: Session, player_id: int, season_id: int):
+    return (
+        db.query(models.PlayerMatchStats)
+        .filter(
+            models.PlayerMatchStats.player_id == player_id,
+            models.PlayerMatchStats.season_id == season_id
+        )
+        .all()
+    )
 
-def createPlayerStats(db: Session, player_stats: schemas.PlayerStatsCreate):
+def createPlayerMatchStats(db: Session, player_stats: schemas.PlayerMatchStatsCreate):
     return player_stats_crud.create(db, player_stats)
 
 
-def updatePlayerStats(
+def updatePlayerMatchStats(
     db: Session,
     player_stats_id: int,
-    player_stats_update: schemas.PlayerStatsUpdate,
+    player_stats_update: schemas.PlayerMatchStatsUpdate,
 ):
     return player_stats_crud.update(db, player_stats_id, player_stats_update)
 
 
-def deletePlayerStats(db: Session, player_stats_id: int):
+def deletePlayerMatchStats(db: Session, player_stats_id: int):
     return player_stats_crud.delete(db, player_stats_id)
