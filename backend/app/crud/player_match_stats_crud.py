3a201b80848a0d0d271215c440cc362373e2ab13
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-18 22:16:50 -0400

    feat: add CRUD operations for PlayerMatchStats and PlayerSeasonStats

---
+++ backend/app/crud/player_match_stats_crud.py
@@ -0,0 +1,55 @@
+from sqlalchemy.orm import Session
+
+from app import models, schemas
+
+from .base import CRUDBase
+
+
+player_match_stats_crud = CRUDBase(models.PlayerMatchStats)
+
+
+def getPlayerMatchStats(db: Session, player_stats_id: int):
+    return player_match_stats_crud.get(db, player_stats_id)
+
+
+def getPlayerMatchStatsList(db: Session, skip: int = 0, limit: int = 100):
+    return player_match_stats_crud.get_multi(db, skip=skip, limit=limit)
+
+
+def get_matches_stats_by_player_and_season(db: Session, player_id: int, season_id: int):
+    return (
+        db.query(models.PlayerMatchStats)
+        .filter(
+            models.PlayerMatchStats.player_id == player_id,
+            models.PlayerMatchStats.season_id == season_id
+        )
+        .all()
+    )
+
+
+def get_stats_by_player_and_match(db: Session, player_id: int, match_id: int):
+    return (
+        db.query(models.PlayerMatchStats)
+        .filter(
+            models.PlayerMatchStats.player_id == player_id,
+            models.PlayerMatchStats.match_id == match_id
+        )
+        .first()
+    )
+
+
+
+def createPlayerMatchStats(db: Session, player_stats: schemas.PlayerMatchStatsCreate):
+    return player_match_stats_crud.create(db, player_stats)
+
+
+def updatePlayerMatchStats(
+    db: Session,
+    player_stats_id: int,
+    player_stats_update: schemas.PlayerMatchStatsUpdate,
+):
+    return player_match_stats_crud.update(db, player_stats_id, player_stats_update)
+
+
+def deletePlayerMatchStats(db: Session, player_stats_id: int):
+    return player_match_stats_crud.delete(db, player_stats_id)
---
+++ backend/app/crud/player_season_stats_crud.py
@@ -0,0 +1,52 @@
+from sqlalchemy.orm import Session
+
+from app import models, schemas
+
+from .base import CRUDBase
+
+
+player_season_stats_crud = CRUDBase(models.PlayerSeasonStats)
+
+
+def getPlayerSeasonStats(db: Session, player_season_stats_id: int):
+    return player_season_stats_crud.get(db, player_season_stats_id)
+
+
+def getPlayerSeasonStatsList(db: Session, skip: int = 0, limit: int = 100):
+    return player_season_stats_crud.get_multi(db, skip=skip, limit=limit)
+
+
+def get_season_stats_by_player_and_season(db: Session, player_id: int, season_id: int):
+    return (
+        db.query(models.PlayerSeasonStats)
+        .filter(
+            models.PlayerSeasonStats.player_id == player_id,
+            models.PlayerSeasonStats.season_id == season_id
+        )
+        .first()
+    )
+
+
+
+def get_stats_by_player(db: Session, player_id: int):
+    return (
+        db.query(models.PlayerSeasonStats)
+        .filter(models.PlayerSeasonStats.player_id == player_id)
+        .all()
+    )
+
+
+def createPlayerSeasonStats(db: Session, player_stats: schemas.PlayerSeasonStatsCreate):
+    return player_season_stats_crud.create(db, obj_in=player_stats)
+
+
+def updatePlayerSeasonStats(
+    db: Session,
+    player_season_stats_id: int,
+    player_stats_update: schemas.PlayerSeasonStatsUpdate,
+):
+    return player_season_stats_crud.update(db, player_season_stats_id, obj_in=player_stats_update)
+
+
+def deletePlayerSeasonStats(db: Session, player_season_stats_id: int):
+    return player_season_stats_crud.delete(db, player_season_stats_id)
