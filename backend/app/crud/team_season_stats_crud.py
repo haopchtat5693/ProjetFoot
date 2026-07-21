f7c7386fb545bb3422b2eae8788b74aef630ba2e
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-21 19:49:07 -0400

    feat: add CRUD operations for team season statistics

---
+++ backend/app/crud/team_season_stats_crud.py
@@ -0,0 +1,44 @@
+from sqlalchemy.orm import Session
+
+from app import models, schemas
+
+from .base import CRUDBase
+
+
+team_season_stats_crud = CRUDBase(models.TeamSeasonStats)
+
+
+def getTeamSeasonStats(db: Session, team_season_stats_id: int):
+    return team_season_stats_crud.get(db, team_season_stats_id)
+
+
+def getTeamSeasonStatsList(db: Session, skip: int = 0, limit: int = 100):
+    return team_season_stats_crud.get_multi(db, skip=skip, limit=limit)
+
+
+def get_team_season_stats_by_team_league_and_season(db: Session, team_id: int, league_id: int, season_id: int):
+    return (
+        db.query(models.TeamSeasonStats)
+        .filter(
+            models.TeamSeasonStats.team_id == team_id,
+            models.TeamSeasonStats.league_id == league_id,
+            models.TeamSeasonStats.season_id == season_id,
+        )
+        .first()
+    )
+
+
+def createTeamSeasonStats(db: Session, team_stats: schemas.TeamSeasonStatsCreate):
+    return team_season_stats_crud.create(db, team_stats)
+
+
+def updateTeamSeasonStats(
+    db: Session,
+    team_season_stats_id: int,
+    team_stats_update: schemas.TeamSeasonStatsUpdate,
+):
+    return team_season_stats_crud.update(db, team_season_stats_id, team_stats_update)
+
+
+def deleteTeamSeasonStats(db: Session, team_season_stats_id: int):
+    return team_season_stats_crud.delete(db, team_season_stats_id)
