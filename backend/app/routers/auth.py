0759b0811fec49c5ed80f092785304b538a34626
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-27 16:52:51 -0400

    fix: update login function to handle token expiration and adjust access token creation

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -3,11 +3,11 @@ from app.core.deps import get_current_user, oauth2_scheme
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from datetime import timedelta
+from datetime import datetime, timedelta, timezone
 from app.database import get_db
 from app import crud
 from app.core.security import verify_password, create_access_token
-from app.models import User
+from app.models import User, Token
 
 router = APIRouter(tags=["Auth"])
 
@@ -17,6 +17,7 @@ def login(
     form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
 ):
 
+
     user = crud.user_crud.get_user_by_username(db, username=form_data.username)
 
     if not user or not verify_password(form_data.password, user.hashed_password):
@@ -24,11 +25,22 @@ def login(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Identifiants incorrects"
         )
 
+
+    db.query(Token).filter(
+        Token.user_id == user.id,
+        Token.expires_at < datetime.now(timezone.utc)
+        ).delete()
+    db.commit()
+
+    access_token_expires = timedelta(minutes=30)
+    expires_at = datetime.now(timezone.utc) + access_token_expires
+
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=timedelta(minutes=30)
+        data={"sub": user.username}, expires_at=expires_at
     )
 
-    token_in = schemas.TokenCreate(token=access_token, user_id=user.id)
+    token_in = schemas.TokenCreate(token=access_token, user_id=user.id, expires_at=expires_at)
+
     crud.auth_token_crud.create_token(db, token_in)
 
     return {"access_token": access_token, "token_type": "bearer"}
