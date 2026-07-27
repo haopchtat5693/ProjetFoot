fabafa89518dbd858ea93b80b533d80193f04cca
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-27 16:12:38 -0400

    feat: remove email field from User model

--- backend/app/models.py
+++ backend/app/models.py
@@ -112,7 +112,6 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     role = Column(String, default=VIEWER)
 
