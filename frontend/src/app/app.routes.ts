f9cd2c8e465db704ed4ce05b7c800c3a9e1e485c
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-26 20:18:29 -0400

    chore: define application routes for Home, Login, and NotFound components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { NotFound } from './pages/not-found/not-found';
+
+export const routes: Routes = [
+	{
+		path: '',
+		component: Home,
+		pathMatch: 'full',
+	},
+	{
+		path: 'login',
+		component: Login,
+	},
+	{
+		path: '**',
+		component: NotFound,
+	},
+];
