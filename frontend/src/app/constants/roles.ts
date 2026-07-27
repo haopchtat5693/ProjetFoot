fac5f4b6bb10e4066a7a3081c65ae824e585446a
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-07-26 20:18:46 -0400

    chore: add role constants for user permissions

---
+++ frontend/src/app/constants/roles.ts
@@ -0,0 +1,11 @@
+export const ROLE_VIEWER = 'viewer';
+export const ROLE_MANAGER = 'manager';
+export const ROLE_ADMIN = 'admin';
+
+export const DEFAULT_ROLE = ROLE_VIEWER;
+
+export const ROLE_OPTIONS = [
+	{ label: 'Viewer', value: ROLE_VIEWER },
+	{ label: 'Manager', value: ROLE_MANAGER },
+	{ label: 'Admin', value: ROLE_ADMIN },
+];
