bb475d244ca57b350dfea4e1dbc3607aff0df9d6
Youcef Tahri <tahri.youcef2006@gmail.com>
2026-08-01 14:10:48 -0400

    feat: create stadiums component with template and styles

---
+++ frontend/src/app/pages/stadiums/stadiums.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stadiums',
+  imports: [],
+  templateUrl: './stadiums.html',
+  styleUrl: './stadiums.scss',
+})
+export class Stadiums {}
